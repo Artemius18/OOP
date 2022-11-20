2c8d6b6818adf92a11b8eae759cad3ee4a6ba6c1
Artyom Pshenko <artempsenko@gmail.com>
2022-11-20 18:08:30 +0300

    added generalized interface with operations add, remove, show

---
+++ Lab_7/Lab_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    interface IFunction<T>
+    {
+        void Add(T element);
+        void Remove(int pos);
+        void Show();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
