ffcc060113704deb6041ea4260430a74a23ddc85
Artyom Pshenko <artempsenko@gmail.com>
2022-12-04 21:36:01 +0300

    added methods for string: remove, removeSpace, upper, lower, addSymbol

---
+++ Lab_8/Lab_8/String.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Lab09
+{
+    class StringRe
+    {
+        public static string Remove(string str)
+        {
+            str = str.Replace(".", string.Empty);
+            str = str.Replace(",", string.Empty);
+            str = str.Replace("!", string.Empty);
+            str = str.Replace("?", string.Empty);
+            return str;
+        }
+
+
+        public static string AddToString(string str)
+        {
+            return str += "символ";
+        }
+
+
+        public static string RemoveSpace(string str)
+        {
+            return str.Replace(" ", string.Empty);
+        }
+
+
+        public static string Upper(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                str = str.Replace(str[i], char.ToUpper(str[i]));
+            }
+            return str;
+        }
+
+
+        public static string Lower(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                str = str.Replace(str[i], char.ToLower(str[i]));
+            }
+            return str;
+        }
+    }
+}
