2437fd4127479cfc7dea41d4d51531293b487013
Artyom Pshenko <artempsenko@gmail.com>
2022-12-04 18:49:32 +0300

    created lab8

---
+++ Lab_8/Lab_8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Test? test = Hello;
+
+        test();
+        void Hello() => Console.WriteLine("Hello");
+    }
+
+    delegate void Test();
+}
